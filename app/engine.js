88fe1f55a1530f5d47ffb317ebec7c3d52f4dcac
Dez <diddon@phorest.com>
2015-07-12 14:23:17 +0100

    Init commit.

---
+++ app/engine.js
@@ -0,0 +1,70 @@
+/***************************************************
+ *                      Engine
+ ****************************************************
+ *
+ *   About:  App core
+ *
+ ****************************************************/
+var mongo = require('mongojs'),
+    DBcreds = process.env.DB,
+    fs = require('fs');
+
+
+// Recursively iterate over file system (BEWARE: NOT ASYNC!!!)
+var traverseFileSystem = function (currentPath, func) {
+    var files = fs.readdirSync(currentPath),
+        currentFile, stats, key;
+
+    for (key in files) {
+        currentFile = currentPath + '/' + files[key];
+        stats = fs.statSync(currentFile);
+        if (stats.isFile()) {
+            if (func) {
+                func(currentFile, currentPath)
+            }
+        } else if (stats.isDirectory()) {
+            traverseFileSystem(currentFile, func);
+        }
+    }
+};
+
+
+// Import base libraries
+traverseFileSystem('./app/libs/', function (currentFile) {
+    if (currentFile.indexOf('.js') > -1) {
+        require('.' + currentFile);
+    }
+});
+
+
+// Import app components
+traverseFileSystem('./app/components/', function (currentFile) {
+    if (currentFile.indexOf('.js') > -1) {
+        require('.' + currentFile);
+    }
+});
+
+
+// Optional DB
+if (DBcreds) {
+    DB = {};
+    DBcollection = [];
+    objectId = mongo.ObjectId;
+
+
+    // Ask components for their DB requirements
+    EVENTS.emit('DBsetup');
+
+
+    // Spin up DB
+    setTimeout(function () {
+        DB = mongo.connect(DBcreds, DBcollection);
+        EVENTS.emit('DBReady');
+    }, 222);
+}
+
+
+// Optimise static assets
+if (!process.env.DEV) {
+    require('simpl3s').speedify('./www');
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+var port = process.env.PORT || process.env.port || 8081,
+    eventEmitter = require('events');
+
+
+// Global event emitter
+EVENTS = new eventEmitter.EventEmitter();
+
+
+// Import
+require('./app/engine.js');
+
+
+// Listen for HTTP requests
+require('http').createServer(function (req, res) {
+    EVENTS.emit('hit', {
+        req                 : req,
+        res                 : res,
+        requestStarted      : Date.now(),
+        httpStatus          : 200,
+        httpHeaderSent      : false
+    });
+}).listen(port);
+
+console.log('Now listening on port ' + port);
